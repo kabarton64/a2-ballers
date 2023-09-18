0863d286777e6de93918e75a05af50d2061e4174
kabarton64 <22kabarton@gmail.com>
2023-09-18 09:16:18 -0500

    Changed test case to a date

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from diary import diary
 def main():
     d = diary("Diary_1")
     print("Diary made")
-    d.add_entry("First", "First entry!")
+    d.add_entry("Jan. 01 2023", "First entry!")
     print("Entry made")
-    d.rem_entry("First")
+    d.rem_entry("Jan. 01 2023")
     print("Entry removed")
 main()
