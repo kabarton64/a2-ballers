0616ba530b8d081465f2fdf53a58f39f3069c3da
CarsonKrog <carsonkrogulski@Carsons-MacBook-Pro.local>
2023-09-19 11:13:06 -0500

    Fixed formating and one error

--- diary.py
+++ diary.py
@@ -1,38 +1,38 @@
 import os
 
+
 class EntryAlreadyExists(Exception):
-	pass
+    pass
+
 
 class NoEntryFound(Exception):
-	pass
-
+    pass
 
 
 class diary:
-    	def __init__(self, diaryname):
-       		self.diaryname = diaryname
-       		if not os.path.exists(diaryname):
-       			os.makedirs(diaryname)
+    def __init__(self, diaryname):
+        self.diaryname = diaryname
+        if not os.path.exists(diaryname):
+            os.makedirs(diaryname)
 
+    def add_entry(self, title, content):
+        if os.path.exists(self.diaryname + "/" + title + ".txt"):
+            raise EntryAlreadyExists("Entry already exists")
+        else:
+            file = open(self.diaryname + "/" + title + ".txt", "w")
+            file.write(content)
+            file.close()
 
-	def add_entry(self, title, content):
-		if os.path.exists(diaryname + "/" + title + ".txt"):
-			raise EntryAlreadyExists("Entry already exists")
-		else:
-       		 	file = open(self.diaryname + "/" + title + ".txt", "w")
-       		 	file.write(content)
-       		 	file.close()
+    def rem_entry(self, title):
+        if os.path.exists(self.diaryname + "/" + title + ".txt"):
+            os.remove(self.diaryname + "/" + title + ".txt")
+        else:
+            raise NoEntryFound("That entry doesn't exists")
 
-	def rem_entry(self, title):
-		if os.path.exists(self.diaryname + "/" + title + ".txt"):
-        		os.remove(self.diaryname + "/" + title + ".txt")
-		else:
-			raise NoEntryFound("That entry doesn't exists")
-
-    	def read_entry(self, title):
-		if os.path.exists(self.diaryname + "/" + title + ".txt"):
-			file = open(self.diaryname + "/" + title + ".txt", "r")
-			print(file.read())
-			file.close()
-		else:
-			raise NoEntryFound("That entry doesn't exist")
+    def read_entry(self, title):
+        if os.path.exists(self.diaryname + "/" + title + ".txt"):
+            file = open(self.diaryname + "/" + title + ".txt", "r")
+            print(file.read())
+            file.close()
+        else:
+            raise NoEntryFound("That entry doesn't exist")
